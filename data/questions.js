06f8b0a5a14c177018b837283e9a4d271832c933
praveenasg <praveenasg98@gmail.com>
2023-12-14 12:58:59 -0500

    Save to Git - 1

---
+++ data/questions.js
@@ -0,0 +1,334 @@
+export default [
+    {
+        quiz: 
+        [
+            {
+                "_id": "6556864fef59cf14e321594f",
+                "user_id": 20,
+                "quiz_id": 930,
+                "quiz_title": "Adventure Squad",
+                "assigned_to_user_id": "",
+                "questions": [
+                    {
+                        "question_type": "Multiple-Choice",
+                        "question_id": 1,
+                        "question_text": "What's your favourite weekend plan?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "Attending a concert",
+                                "option_weight": 5,
+                                "_id": "6556864fef59cf14e3215951"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "Watching a movie",
+                                "option_weight": 5,
+                                "_id": "6556864fef59cf14e3215952"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "Hiking",
+                                "option_weight": 0,
+                                "_id": "6556864fef59cf14e3215953"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "Trying a new restaurant",
+                                "option_weight": 0,
+                                "_id": "6556864fef59cf14e3215954"
+                            }
+                        ],
+                        "_id": "6556864fef59cf14e3215950"
+                    },
+                    {
+                        "question_type": "Open-Ended",
+                        "question_id": 2,
+                        "question_text": "Which of these sounds most motivating?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "Discovery, comfort, adventures, diversity, visionary option_ideas",
+                                "option_weight": 6,
+                                "_id": "6556864fef59cf14e3215956"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "Passion, glory, willpower, teamwork, intuition, history",
+                                "option_weight": 4,
+                                "_id": "6556864fef59cf14e3215957"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "Opportunities, insights, freedom, prosperity, sucess",
+                                "option_weight": 0,
+                                "_id": "6556864fef59cf14e3215958"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "Security, expertise, possibilities, harmny, self-growth",
+                                "option_weight": 0,
+                                "_id": "6556864fef59cf14e3215959"
+                            }
+                        ],
+                        "_id": "6556864fef59cf14e3215955"
+                    },
+                    {
+                        "question_type": "Likert-Scale",
+                        "question_id": null,
+                        "question_text": "Flexible?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "6556864fef59cf14e321595b"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "6556864fef59cf14e321595c"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "6556864fef59cf14e321595d"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "6556864fef59cf14e321595e"
+                            }
+                        ],
+                        "_id": "6556864fef59cf14e321595a"
+                    }
+                ],
+                "__v": 0
+            },
+            {
+                "_id": "65568b61ef59cf14e3215990",
+                "user_id": 20,
+                "quiz_id": 931,
+                "quiz_title": "Extended time",
+                "assigned_to_user_id": "",
+                "questions": [
+                    {
+                        "question_type": "Multiple-Choice",
+                        "question_id": 1,
+                        "question_text": "Which of these could you discuss for an extended time?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "Emotions and option_ideas but not physical sensations (healthcare, dieting)",
+                                "option_weight": 5,
+                                "_id": "65568b61ef59cf14e3215992"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "Disuss emotions and phycical sensations but no theories (abstract concepts)",
+                                "option_weight": 5,
+                                "_id": "65568b61ef59cf14e3215993"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "Discuss physical sensations and theories but not diverse",
+                                "option_weight": 0,
+                                "_id": "65568b61ef59cf14e3215994"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "Emotional states",
+                                "option_weight": 0,
+                                "_id": "65568b61ef59cf14e3215995"
+                            }
+                        ],
+                        "_id": "65568b61ef59cf14e3215991"
+                    },
+                    {
+                        "question_type": "Open-Ended",
+                        "question_id": 2,
+                        "question_text": "When exchanging opinions:",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "I love to have multiple layers to a conversation even if we go around in circles",
+                                "option_weight": 6,
+                                "_id": "65568b61ef59cf14e3215997"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "I would sooner get to the point, so we have time for other pursuits",
+                                "option_weight": 4,
+                                "_id": "65568b61ef59cf14e3215998"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "I do not over-analyze emotional states. I would rather share good vibes",
+                                "option_weight": 0,
+                                "_id": "65568b61ef59cf14e3215999"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "I am keen to explore many psychological dimensions",
+                                "option_weight": 0,
+                                "_id": "65568b61ef59cf14e321599a"
+                            }
+                        ],
+                        "_id": "65568b61ef59cf14e3215996"
+                    },
+                    {
+                        "question_type": "Likert-Scale",
+                        "question_id": null,
+                        "question_text": "Emotional?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b61ef59cf14e321599c"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b61ef59cf14e321599d"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b61ef59cf14e321599e"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b61ef59cf14e321599f"
+                            }
+                        ],
+                        "_id": "65568b61ef59cf14e321599b"
+                    }
+                ],
+                "__v": 0
+            },
+            {
+                "_id": "65568b75ef59cf14e32159c0",
+                "user_id": 20,
+                "quiz_id": 932,
+                "quiz_title": "Conversations",
+                "assigned_to_user_id": "",
+                "questions": [
+                    {
+                        "question_type": "Multiple-Choice",
+                        "question_id": 1,
+                        "question_text": "What are you looking for in conversations?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "For fun and authentic breif conversations",
+                                "option_weight": 5,
+                                "_id": "65568b75ef59cf14e32159c2"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "To get a scoop fo new perspectives and psychological impressions",
+                                "option_weight": 5,
+                                "_id": "65568b75ef59cf14e32159c3"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "For potential long-term conectons",
+                                "option_weight": 0,
+                                "_id": "65568b75ef59cf14e32159c4"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "Networking for collaborations- lets do things together!",
+                                "option_weight": 0,
+                                "_id": "65568b75ef59cf14e32159c5"
+                            }
+                        ],
+                        "_id": "65568b75ef59cf14e32159c1"
+                    },
+                    {
+                        "question_type": "Open-Ended",
+                        "question_id": 2,
+                        "question_text": "What are you looking for right now?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "Let us have a great discussion",
+                                "option_weight": 6,
+                                "_id": "65568b75ef59cf14e32159c7"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "For a brief, engaging chat",
+                                "option_weight": 4,
+                                "_id": "65568b75ef59cf14e32159c8"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "For a brief, engaging chat",
+                                "option_weight": 0,
+                                "_id": "65568b75ef59cf14e32159c9"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "For a brief, engaging chat",
+                                "option_weight": 0,
+                                "_id": "65568b75ef59cf14e32159ca"
+                            }
+                        ],
+                        "_id": "65568b75ef59cf14e32159c6"
+                    },
+                    {
+                        "question_type": "Likert-Scale",
+                        "question_id": null,
+                        "question_text": "Organized?",
+                        "question_weight": null,
+                        "options": [
+                            {
+                                "option_id": 1,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b75ef59cf14e32159cc"
+                            },
+                            {
+                                "option_id": 2,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b75ef59cf14e32159cd"
+                            },
+                            {
+                                "option_id": 3,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b75ef59cf14e32159ce"
+                            },
+                            {
+                                "option_id": 4,
+                                "option_text": "",
+                                "option_weight": null,
+                                "_id": "65568b75ef59cf14e32159cf"
+                            }
+                        ],
+                        "_id": "65568b75ef59cf14e32159cb"
+                    }
+                ],
+                "__v": 0
+            }
+        ]
+    }
+  ]
